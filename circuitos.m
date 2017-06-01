f5251fcb8b2892f5ce3b8d503e04694ee93774d0
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2017-05-31 22:44:47 -0300

    Otimização de circuitos.m

--- circuitos.m
+++ circuitos.m
@@ -3,7 +3,7 @@
 ## Autor: Matheus de Sousa Siqueira <matheussiqueira.ec@gmail.com>
 ## Data: 31/05/2017
 
-function [variaveis] = circuitos(coeficientes, solucoes, unidade, variaveis)
+function circuitos(coeficientes, solucoes, unidade, variaveis)
 	% Uso:	
 	%     circuitos(coeficientes, solucoes, unidade, variaveis)
 	%
@@ -28,15 +28,16 @@ function [variaveis] = circuitos(coeficientes, solucoes, unidade, variaveis)
 	if !(ischar(variaveis) && unidade)
 		error("O vetor de variáveis e/ou a unidade devem ser compostos de caracteres.");
 	endif
-	
-	aux = size(coeficientes, 1);
-	variaveis = cellstr(variaveis);
-	x = ones(aux,1);
-	a_det = det(coeficientes);
 
-	for i = 1:aux
-		C = coeficientes;
-		C( : ,i) = solucoes;
-		variaveis(i,1) = strcat(variaveis(i,1), {" = "}, num2str(det(C)/a_det), {" "}, unidade);
+	if (size(solucoes, 1) == 1)
+		solucoes = solucoes.';
+	endif
+	
+	variaveis = cellstr(variaveis);
+
+	x = coeficientes\solucoes;
+
+	for i = 1:size(coeficientes, 1)
+		printf("%s = %f %s\n", variaveis{i}, x(i), unidade);
 	end
 endfunction
